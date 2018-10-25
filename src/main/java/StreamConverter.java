4610c2b97875dae41f900782d8f25b586bb1dc22
Sarmad <sarmadsa2@gmail.com>
2018-10-25 14:04:50 +0200

    Implement stream converter

---
+++ src/main/java/StreamConverter.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class StreamConverter {
+
+    public String convertStreamToString(InputStream is) {
+        BufferedReader in = new BufferedReader(new InputStreamReader(is));
+        StringBuilder response = new StringBuilder();
+        try {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+                response.append('\n');
+            }
+        } catch (IOException ex) {
+            System.out.println("Could not read the data from HTTP response: " + ex.getMessage());
+        }
+        return response.toString();
+    }
+}
